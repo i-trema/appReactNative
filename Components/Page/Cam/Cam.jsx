e84c485f7d8503eca2ead2ffea7e64bc62c90ea4
Ianis Mauraisin <tsaitrema@hotmail.fr>
2022-06-02 14:39:00 +0200

    yarn

--- Components/Page/Cam/Cam.jsx
+++ Components/Page/Cam/Cam.jsx
@@ -1,16 +1,53 @@
-import React, { Component } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, ActivityIndicator } from "react-native";
+import { Camera, CameraType } from "expo-camera";
+import { useEffect, useState } from "react";
+import { STYLES_VARIABLES } from "../../../variables/stylesVariables";
+import { Feather } from "@expo/vector-icons";
 
-const Cam = () => {
+const Cam = ({ route, navigation }) => {
+  const [cameraPermission, setCameraPermission] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      let permission = await Camera.requestCameraPermissionsAsync();
+      setCameraPermission(permission.granted);
+    })();
+  }, []);
+
+  if (cameraPermission === null) {
+    return (
+      <View style={styles.container}>
+        <ActivityIndicator size={64} color={STYLES_VARIABLES.PRIMARY_COLOR} />
+      </View>
+    );
+  }
+
+  if (cameraPermission === false) {
+    return (
+      <View style={styles.container}>
+        <Text>Permission refusée</Text>
+        <Feather
+          name="camera-off"
+          size={64}
+          color={STYLES_VARIABLES.DANGER_COLOR}
+        />
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Cam</Text>
+      <Camera style={{ width: "100%", height: "100%" }}></Camera>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    height: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+  },
 });
 
 export default Cam;
