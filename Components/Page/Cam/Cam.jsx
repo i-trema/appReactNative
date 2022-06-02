d5e1d23a26a9f49e4eef341fb3a64eb7b81c189d
Ianis Mauraisin <tsaitrema@hotmail.fr>
2022-06-02 16:31:46 +0200

    picture

--- Components/Page/Cam/Cam.jsx
+++ Components/Page/Cam/Cam.jsx
@@ -1,16 +1,43 @@
-import { View, Text, StyleSheet, ActivityIndicator } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  ActivityIndicator,
+  useWindowDimensions,
+  TouchableOpacity,
+} from "react-native";
 import { Camera, CameraType } from "expo-camera";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { STYLES_VARIABLES } from "../../../variables/stylesVariables";
 import { Feather } from "@expo/vector-icons";
+import { MaterialIcons } from "@expo/vector-icons";
 
 const Cam = ({ route, navigation }) => {
   const [cameraPermission, setCameraPermission] = useState(null);
+  const [cameraType, setCameraType] = useState(CameraType.back);
+  const [isFlashOn, setIsFlashOn] = useState(false);
+  const cameraRef = useRef();
+
+  function toggleCameraType() {
+    setCameraType(
+      cameraType === CameraType.back ? CameraType.front : CameraType.back
+    );
+  }
+
+  function toggleFlash() {
+    setIsFlashOn(!isFlashOn);
+  }
+
+  const sizes = useWindowDimensions();
 
   useEffect(() => {
     (async () => {
-      let permission = await Camera.requestCameraPermissionsAsync();
-      setCameraPermission(permission.granted);
+      try {
+        let permission = await Camera.requestCameraPermissionsAsync();
+        setCameraPermission(permission.granted);
+      } catch (err) {
+        console.log(err);
+      }
     })();
   }, []);
 
@@ -35,9 +62,51 @@ const Cam = ({ route, navigation }) => {
     );
   }
 
+  async function takePicture() {
+    const picture = cameraRef.current.takePictureAsync();
+    console.log(picture);
+    //1- Utiliser le contexte pour mettre picture dans avatar de user.
+
+    //2- Retourner en arriere (Profil)
+  }
+
   return (
     <View style={styles.container}>
-      <Camera style={{ width: "100%", height: "100%" }}></Camera>
+      <Camera
+        ref={cameraRef}
+        flashMode={isFlashOn ? "torch" : "off"}
+        type={cameraType}
+        ratio="16:9"
+        style={{ width: sizes.width, height: (sizes.width * 16) / 9 }}
+      >
+        <View style={styles.iconsContainer}>
+          <TouchableOpacity onPress={toggleCameraType}>
+            <MaterialIcons
+              name="flip-camera-android"
+              size={64}
+              color={STYLES_VARIABLES.SECONDARY_COLOR}
+            ></MaterialIcons>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={toggleFlash}>
+            <MaterialIcons
+              name={isFlashOn ? "flash-on" : "flash-off"}
+              size={50}
+              color={
+                isFlashOn
+                  ? STYLES_VARIABLES.SUCCESS_COLOR
+                  : STYLES_VARIABLES.DANGER_COLOR
+              }
+            ></MaterialIcons>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={takePicture}>
+            <MaterialIcons
+              name="camera"
+              size={64}
+              color={STYLES_VARIABLES.SUCCESS_COLOR}
+            ></MaterialIcons>
+          </TouchableOpacity>
+        </View>
+      </Camera>
     </View>
   );
 };
@@ -47,6 +116,18 @@ const styles = StyleSheet.create({
     height: "100%",
     justifyContent: "center",
     alignItems: "center",
+  },
+  iconsContainer: {
+    display: "flex",
+    flexDirection: "row",
+    width: "80%",
+    backgroundColor: "rgba(0,0,0,0.2)",
+    justifyContent: "space-around",
+    alignSelf: "center",
+    position: "absolute",
+    bottom: 100,
+    borderRadius: 500,
+    padding: 10,
   },
 });
 
